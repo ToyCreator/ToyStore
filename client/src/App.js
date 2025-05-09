bcce294f0e11397ff22831551ff095a7fa496a7a
王文杰 <421804310@qq.com>
2025-05-08 20:47:26 -0700

    更改hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./router/index";
-
+import { HashRouter } from 'react-router-dom';
+// 从 BrowserRouter 改为 HashRouter。
+// GitHub Pages 服务器只看 index.html，不会管 #/home，由前端解析路径，
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
